040585804c24c3d0fe2dfbf7bcb3645a5f9f38df
salihgursoy61 <121294952+salihgursoy61@users.noreply.github.com>
2024-11-20 17:49:47 +0300

    sorular olusturuldu ve sorular sayfa ustunde gosterildi

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
-console.log(soruListesi[0].cevabiKontrolEt("d"));
-console.log(soruListesi[1].cevabiKontrolEt("d"));
-console.log(soruListesi[2].cevabiKontrolEt("a"));
+const soruListesi = [
+  new Soru(
+    "1-hangisi javascript paket yonetim uygulamasıdır?",
+    { a: "node js", b: "typescript", c: "nuget", d: "npm" },
+    "d"
+  ),
+  new Soru(
+    "2-hangisi frontend kapsamında değerlendirilmez?",
+    { a: "css", b: "Html", c: "javascript", d: "sql" },
+    "d"
+  ),
+  new Soru(
+    "3-hangisi backend kapsamında değerlendirlir?",
+    { a: "node js", b: "TypeScript", c: "angular", d: "react" },
+    "a"
+  ),
+  new Soru(
+    "4-hangisi javascript programlama dilini kullanmaz?",
+    { a: "react", b: "angular", c: "vuejs", d: "asp.net" },
+    "d"
+  ),
+];
+
+const quiz = new Quiz(soruListesi);
+const ui = new UI();
+
+console.log(quiz);
+
+document.getElementById("BtnSoruGetir").addEventListener("click", function () {
+  if (quiz.sorular.length != quiz.soruIndex) {
+    ui.soruGoster(quiz.soruGetir())
+    quiz.soruIndex += 1;
+    console.log(quiz);
+  } else {
+    console.log("quiz bitti");
+  }
+});

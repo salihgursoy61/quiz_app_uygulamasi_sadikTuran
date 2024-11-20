309ce6e4d403691a855d21208714b90d2633bf2c
salihgursoy61 <121294952+salihgursoy61@users.noreply.github.com>
2024-11-20 19:55:12 +0300

    diğer soruya gecmek icin buton eklendi ve kacıncı soruda ve toplam kac sorunun oldugunu görmek icinde bootsrap librarisinden badge eklendi

--- js/app.js
+++ js/app.js
@@ -26,25 +26,32 @@ const ui = new UI();
 
 console.log(quiz);
 
-document.getElementById("BtnSoruGetir").addEventListener("click", function () {
+ui.btnNext.addEventListener("click", function () {
   if (quiz.sorular.length != quiz.soruIndex) {
     ui.soruGoster(quiz.soruGetir());
-    console.log(quiz);
+    ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
   } else {
     console.log("quiz bitti");
   }
 });
 
 function optionSelected(e) {
+  let selectedElement = e.target;
+
+  if (selectedElement.nodeName == "SPAN") {
+    selectedElement = selectedElement.parentElement;
+  }
+
   const cevap = e.target.textContent[0];
   const soru = quiz.soruGetir();
 
   if (soru.cevabiKontrolEt(cevap)) {
-    e.target.classList.add("correct");
-    e.target.insertAdjacentHTML("beforeend", ui.correctIcon);
+    quiz.dogruCevapSayisi += 1;
+    selectedElement.classList.add("correct");
+    selectedElement.insertAdjacentHTML("beforeend", ui.correctIcon);
   } else {
-    e.target.classList.add("incorrect");
-    e.target.insertAdjacentHTML("beforeend", ui.inCorrectIcon);
+    selectedElement.classList.add("incorrect");
+    selectedElement.insertAdjacentHTML("beforeend", ui.inCorrectIcon);
   }
   quiz.soruIndex += 1;
   ui.disabledAllOption();
